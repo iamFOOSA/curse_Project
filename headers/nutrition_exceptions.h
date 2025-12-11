c75c6fe0d87e7a6ed390bc2caf1c696bfc83b6ab
Данила Абрамчук <abramchukdanila@gmail.com>
2025-12-11 19:56:32 +0300

    commit curseP 0_4

--- headers/nutrition_exceptions.h
+++ headers/nutrition_exceptions.h
@@ -5,15 +5,19 @@
 #include <exception>
 
 class NutritionException : public std::exception {
-protected:
+private:
     std::string message;
     std::string context;
+
+protected:
+    std::string get_message_internal() const { return message; }
+    std::string get_context_internal() const { return context; }
 
 public:
     explicit NutritionException(const std::string& msg, const std::string& ctx = "")
         : message(msg), context(ctx) {}
 
-    virtual ~NutritionException() noexcept = default;
+    ~NutritionException() noexcept override = default;
 
     const char* what() const noexcept override {
         return message.c_str();
@@ -21,7 +25,7 @@ public:
 
     std::string get_message() const { return message; }
     std::string get_context() const { return context; }
-    std::string get_full_message() const {
+    virtual std::string get_full_message() const {
         if (context.empty()) {
             return message;
         }
@@ -38,13 +42,14 @@ public:
         : NutritionException(msg, ctx), filename(fname) {}
 
     std::string get_filename() const { return filename; }
-    std::string get_full_message() const {
-        std::string full = message;
+    std::string get_full_message() const override {
+        std::string full = get_message_internal();
         if (!filename.empty()) {
             full += " [File: " + filename + "]";
         }
-        if (!context.empty()) {
-            full += " [Context: " + context + "]";
+        std::string ctx = get_context_internal();
+        if (!ctx.empty()) {
+            full += " [Context: " + ctx + "]";
         }
         return full;
     }
@@ -59,13 +64,14 @@ public:
         : NutritionException(msg, ctx), dataType(dtype) {}
 
     std::string get_data_type() const { return dataType; }
-    std::string get_full_message() const {
-        std::string full = message;
+    std::string get_full_message() const override {
+        std::string full = get_message_internal();
         if (!dataType.empty()) {
             full += " [Data Type: " + dataType + "]";
         }
-        if (!context.empty()) {
-            full += " [Context: " + context + "]";
+        std::string ctx = get_context_internal();
+        if (!ctx.empty()) {
+            full += " [Context: " + ctx + "]";
         }
         return full;
     }
@@ -82,16 +88,17 @@ public:
 
     std::string get_field_name() const { return fieldName; }
     std::string get_invalid_value() const { return invalidValue; }
-    std::string get_full_message() const {
-        std::string full = message;
+    std::string get_full_message() const override {
+        std::string full = get_message_internal();
         if (!fieldName.empty()) {
             full += " [Field: " + fieldName + "]";
         }
         if (!invalidValue.empty()) {
             full += " [Value: " + invalidValue + "]";
         }
-        if (!context.empty()) {
-            full += " [Context: " + context + "]";
+        std::string ctx = get_context_internal();
+        if (!ctx.empty()) {
+            full += " [Context: " + ctx + "]";
         }
         return full;
     }
@@ -118,13 +125,14 @@ public:
         : NutritionException(msg, ctx), calculationType(calcType) {}
 
     std::string get_calculation_type() const { return calculationType; }
-    std::string get_full_message() const {
-        std::string full = message;
+    std::string get_full_message() const override {
+        std::string full = get_message_internal();
         if (!calculationType.empty()) {
             full += " [Calculation: " + calculationType + "]";
         }
-        if (!context.empty()) {
-            full += " [Context: " + context + "]";
+        std::string ctx = get_context_internal();
+        if (!ctx.empty()) {
+            full += " [Context: " + ctx + "]";
         }
         return full;
     }
